1b4b19e0454d4b45b6c49ecd4f28f2566c49371b
taylorldauphinee <59611079+taylorldauphinee@users.noreply.github.com>
2021-06-21 07:06:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Program.cs
+/// June 21, 2021
+/// Written by Taylor Dauphinee
+/// A program that plays the game fizzbuzz
+/// </summary>
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1, end = 100; // These values can be changed if necessary
+
+            // run the game from start to end
+            for (int i = start; i <= end; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                if (output == "")
+                {
+                    output += i.ToString();
+                }
+                Console.WriteLine(output);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(true);
+        }
+    }
+}
